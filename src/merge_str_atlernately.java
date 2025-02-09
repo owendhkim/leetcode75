b1bdc729e76abc5c9994d6ffdf8ddeb3f8b6886b
owendhkim <owenk@iastate.edu>
2025-02-08 22:55:23 -0600

    add merge string alternately

---
+++ src/merge_str_atlernately.java
@@ -0,0 +1,12 @@
+public class merge_str_atlernately
+{
+    public String mergeAlternately(String word1, String word2) {
+
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
