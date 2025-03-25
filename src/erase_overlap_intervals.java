aa58b276e4fa3adf80b8b456647572b243705d72
owendhkim <owenk@iastate.edu>
2025-03-25 12:49:17 -0500

    Erase overlapping intervals done

--- src/erase_overlap_intervals.java
+++ src/erase_overlap_intervals.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
-public class erase_overlap_intervals {
+public class erase_overlap_intervals
+{
+    public int eraseOverlapIntervals(int[][] intervals)
+    {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1],b[1]));
+        int end = Integer.MIN_VALUE;
+        int count = 0;
+        for(int[] arr : intervals)
+        {
+            if(arr[0] >= end)
+            {
+                count++;
+                end = arr[1];
+            }
+        }
+        return intervals.length - count;
+    }
 }
