505602cca0986a0dc7d367e8cfad36ddb511929f
owendhkim <owenk@iastate.edu>
2025-04-07 20:06:16 -0500

    1431. Kids With the Greatest Number of Candies done

--- src/kids_with_greatest_num_of_candies.java
+++ src/kids_with_greatest_num_of_candies.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class kids_with_greatest_num_of_candies {
+public class kids_with_greatest_num_of_candies
+{
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies)
+    {
+        List<Boolean> l = new ArrayList<>();
+        int max = 0;
+        for(int n : candies)
+        {
+            if(n > max) max = n;
+        }
+        for(int i = 0; i < candies.length; i++)
+        {
+            if(candies[i] + extraCandies < max)
+            {
+                l.add(false);
+            }
+            else l.add(true);
+        }
+        return l;
+    }
 }
