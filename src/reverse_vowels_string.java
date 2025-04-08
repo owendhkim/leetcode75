b83806a008dc0dfc810bac62deaed687a5bec856
owendhkim <owenk@iastate.edu>
2025-04-07 20:05:48 -0500

    345. Reverse Vowels of a String done

--- src/reverse_vowels_string.java
+++ src/reverse_vowels_string.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+public class reverse_vowels_string
+{
+    public String reverseVowels(String s)
+    {
+        int left = 0;
+        int right = s.length()-1;
+        char[] str = s.toCharArray();
+        while(left < right)
+        {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
 
-public class reverse_vowels_string {
+            if(isVowel(l) && isVowel(r))
+            {
+                char temp = str[left];
+                str[left] = str[right];
+                str[right] = temp;
+                left++;
+                right--;
+
+            }
+            else if(!isVowel(l))
+            {
+                left++;
+            }
+            else if(!isVowel(r))
+            {
+                right--;
+            }
+        }
+        return new String(str);
+
+    }
+    public boolean isVowel(char c)
+    {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
 }
