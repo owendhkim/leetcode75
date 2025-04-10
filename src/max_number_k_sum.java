63b0ef1eafdb88dd4700f75a028e5bd4efe18330
owendhkim <owenk@iastate.edu>
2025-04-09 22:44:58 -0500

    1679. Max Number of K-Sum Pairs done

--- src/max_number_k_sum.java
+++ src/max_number_k_sum.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
-public class max_number_k_sum {
+public class max_number_k_sum
+{
+    public int maxOperations(int[] nums, int k)
+    {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length-1;
+        int count = 0;
+
+        while(left<right)
+        {
+            if(nums[left] + nums[right] == k)
+            {
+                count++;
+                left++;
+                right--;
+            }
+            else if(nums[left] + nums[right] < k)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return count;
+    }
 }
