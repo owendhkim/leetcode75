c28cce40319ee54b05c7eb6352df4c6304672541
owendhkim <owenk@iastate.edu>
2025-04-09 22:44:44 -0500

    string_compression done

--- src/string_compression.java
+++ src/string_compression.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
-public class string_compression {
+public class string_compression
+{
+    public int compress(char[] chars)
+    {
+        if(chars.length == 1) return 1;
+        char cur = chars[0];
+        int count = 0;
+        int write = 0;
+        for(char c : chars)
+        {
+            if(cur == c)
+            {
+                count++;
+            }
+            else
+            {
+                chars[write++] = cur;
+                if(count > 1)
+                {
+                    String cnt = String.valueOf(count);
+                    for(char digit : cnt.toCharArray())
+                    {
+                        chars[write++] = digit;
+                    }
+                }
+                count = 1;
+                cur = c;
+            }
+        }
+        chars[write++] = cur;
+        if(count > 1)
+        {
+            String cnt = String.valueOf(count);
+            for(char digit : cnt.toCharArray())
+            {
+                chars[write++] = digit;
+            }
+        }
+        return write;
+    }
 }
